15e48d0b3704a1e8d7e772c5bcbacda1e80d138b
MohamedAhmedSD <btoox8python@gmail.com>
2023-04-16 10:27:52 +0200

    strat day 6 with products details and adjust main btn

--- lib/views/widgets/main_button.dart
+++ lib/views/widgets/main_button.dart
@@ -1,15 +1,19 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
 
 // customized btn
 // text && function
 class MainButton extends StatelessWidget {
   final String text;
+  final bool hasCircularBorder;
+
   final VoidCallback onTap;
   // final void Function()? onTap;
 
   const MainButton({
     Key? key,
     required this.text,
+    this.hasCircularBorder = false,
     required this.onTap,
   }) : super(key: key);
 
@@ -24,6 +28,11 @@ class MainButton extends StatelessWidget {
         style: ElevatedButton.styleFrom(
           backgroundColor: Theme.of(context).primaryColor,
           // foregroundColor: Theme.of(context).primaryColor,
+          shape: hasCircularBorder
+              ? RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(24.0),
+                )
+              : null,
         ),
         child: Text(
           text,
